d9bd7038fd194b6ad6d397f4162da52649215fd6
Giovanny González <gonzalezgiovanny369@gmail.com>
2023-06-27 11:27:42 -0600

    actualizacion
    se actualizo estilos y se modifico la conexion del index con datos php

--- datos.php
+++ datos.php
@@ -1,8 +1,16 @@
 <?php
+$nombre = $_POST ['nombre'];
+$email = $_POST ['email'];
+$telefono = $_POST ['telefono'];
+$sitioweb = $_POST ['sitio_web'];
+$asunto = $_POST ['asunto'];
+$mensaje = $_POST ['mensaje'];
 
-//$nombre = $_GET ['txtNombre'];
-$nombre = $_POST ['txtNombre'];
-//$apellido = $_GET ['txtApellido'];
-$apellido = $_POST ['txtApellido'];
-echo $nombre. "/" . $apellido;
+/*$nombre = $_GET ['nombre'];
+$email = $_GET ['email'];
+$telefono = $_GET ['telefono'];
+$sitioweb = $_GET ['sitio_web'];
+$asunto = $_GET ['asunto'];
+$mensaje = $_GET ['mensaje'];*/
+echo $nombre. "/" . $email. "/" . $telefono. "/" . $sitioweb. "/" . $asunto. "/" . $mensaje;
 ?>
